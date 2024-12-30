578806fbd1f3f5ad7675d9617d74e231aebdc09a
Rico-febrian <ricofebrian731@gmail.com>
2024-12-30 10:39:01 +0700

    Update database references

--- dags/load_query/load_data_source.sql
+++ dags/load_query/load_data_source.sql
@@ -10,25 +10,25 @@ TRUNCATE TABLE staging.nation;
 TRUNCATE TABLE staging.region;   
 
 CREATE OR REPLACE TABLE staging.customer AS
-SELECT * FROM snowflake_sample_data.tpch_sf1.customer;
+SELECT * FROM source_db.data_source.customer;
 
 CREATE OR REPLACE TABLE staging.supplier AS
-SELECT * FROM snowflake_sample_data.tpch_sf1.supplier;
+SELECT * FROM source_db.data_source.supplier;
 
 CREATE OR REPLACE TABLE staging.orders AS
-SELECT * FROM snowflake_sample_data.tpch_sf1.orders;
+SELECT * FROM source_db.data_source.orders;
 
 CREATE OR REPLACE TABLE staging.lineitem AS
-SELECT * FROM snowflake_sample_data.tpch_sf1.lineitem;
+SELECT * FROM source_db.data_source.lineitem;
 
 CREATE OR REPLACE TABLE staging.part AS
-SELECT * FROM snowflake_sample_data.tpch_sf1.part;
+SELECT * FROM source_db.data_source.part;
 
 CREATE OR REPLACE TABLE staging.partsupp AS
-SELECT * FROM snowflake_sample_data.tpch_sf1.partsupp;
+SELECT * FROM source_db.data_source.partsupp;
 
 CREATE OR REPLACE TABLE staging.nation AS
-SELECT * FROM snowflake_sample_data.tpch_sf1.nation;
+SELECT * FROM source_db.data_source.nation;
 
 CREATE OR REPLACE TABLE staging.region AS
-SELECT * FROM snowflake_sample_data.tpch_sf1.region;
+SELECT * FROM source_db.data_source.region;
